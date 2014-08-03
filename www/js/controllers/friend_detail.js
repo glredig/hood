85f9a8eaef56588e66deb64a258062994b0366b7
John Kelly <mr.johnkelly@live.com>
2014-08-03 16:57:34 -0700

    Use starter sidemenu project instead of tabs
    For some reason if tabs are included in a template they hide the content of the template (see playlists page)

--- www/js/controllers/friend_detail.js
+++
@@ -1,3 +0,0 @@
-app.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
--- www/js/controllers/friends.js
+++
@@ -1,3 +0,0 @@
-app.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
