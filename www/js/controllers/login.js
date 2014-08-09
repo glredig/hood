1db19c7bc05e051d1595ed2696b7464bce1274e7
John Kelly <mr.johnkelly@live.com>
2014-08-09 01:08:02 -0700

    Redirect to map page on sign in

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -1,4 +1,4 @@
-app.controller('LoginCtrl', function ($scope, $location, OpenFB, Facebook, CurrentUser) {
+app.controller('LoginCtrl', function ($scope, $state, OpenFB, Facebook, CurrentUser) {
 
   $scope.facebookLogin = function () {
     OpenFB.login('email,user_birthday').then(
@@ -6,16 +6,16 @@ app.controller('LoginCtrl', function ($scope, $location, OpenFB, Facebook, Curre
         Facebook.save(
           { facebook: { token: window.sessionStorage.fbtoken }},
           function(response) {
-            console.log(response);
+            // console.log(response);
             var user_json = response.current_user;
             CurrentUser.store(user_json);
+            $state.go('app.performers');
           },
           function(response) {
-            console.log(response);
+            // console.log(response);
             // Helpers.ajax_error_handling(response);
           }
         );
-        $location.path('/app/performers');
       },
       function () {
         alert('OpenFB login failed');
