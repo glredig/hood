9d8ec829bb784e489ca0a21601393c4d73f6b8fe
Gabriel Redig <glredig@gmail.com>
2014-08-09 02:26:42 -0700

    Map populates performers (position & popup)
    Performer profile closer to wireframe

--- www/js/controllers/performer.js
+++ www/js/controllers/performer.js
@@ -1,2 +1,12 @@
 app.controller('PerformerCtrl', function($scope, $stateParams) {
+  $scope.performer = { 
+    name: 'DJ Jazzy Jeff', 
+    id: 1, 
+    location: { 
+      lat: 37.393794, 
+      lng: -122.098097 
+    }, 
+    image: "http://releaseboard.com/images/releaseboard_gabe.jpeg",
+    tagline: "80's all night tonight..." 
+  };
 })
--- www/js/controllers/performers.js
+++ www/js/controllers/performers.js
@@ -1,12 +1,33 @@
 app.controller('PerformersCtrl', function($scope) {
   $scope.performers = [
-    { name: 'DJ Jazzy Jeff', id: 1, location: { lat: 37.393794, lng: -122.098097 } },
-    { name: 'Sam Da Man', id: 2, location: { lat: 37.394953, lng: -122.100897 } }
+    { name: 'DJ Jazzy Jeff', id: 1, location: { lat: 37.393794, lng: -122.098097 }, image: "http://releaseboard.com/images/releaseboard_gabe.jpeg" },
+    { name: 'Sam Da Man', id: 2, location: { lat: 37.394953, lng: -122.100897 }, image: "http://releaseboard.com/images/releaseboard_john.png" }
   ];
+
+  $scope.buildMarkers = function() {
+    var markers = {};
+
+    angular.forEach($scope.performers, function(value, key) {
+      var m = "m" + (key + 1);
+
+      markers[m] = {
+        lat: value.location.lat,
+        lng: value.location.lng,
+        focus: false,
+        draggable: false,
+        message: "<img src='" + value.image + "' /><span>" + value.name + "</span><a href='#/app/performers/" + 
+                  value.id + "'> > </a>",
+        icon: {}
+      }
+    });
+
+    return markers;
+  }
 
   angular.extend($scope, {
     center: {
-        autoDiscover: true
-    }
+      autoDiscover: true
+    },
+    markers: $scope.buildMarkers()
   });
 });
